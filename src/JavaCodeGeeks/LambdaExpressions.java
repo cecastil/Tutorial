3b08d56d74ff98ac039a40de1a58ca163b001be2
CESAR CASTILLO <67616992+cecastil@users.noreply.github.com>
2020-11-25 23:45:58 -0600

    Complete LambdaExpression tutorial, but the error keep going

--- src/JavaCodeGeeks/LambdaExpressions.java
+++ src/JavaCodeGeeks/LambdaExpressions.java
@@ -1,20 +1,62 @@
 package JavaCodeGeeks;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LambdaExpressions {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-        s1.setId(123);
-        s1.setName("Cesar");
+        Student s1 = new Student(123,"Cesar");
 
-        Student s2 = new Student();
-        s2.setId(321);
-        s2.setName("Sam");
+        Student s2 = new Student(321,"Samuel");
+
+        List<Student> students = getStudent();
 
         Comparator<Student> byId = Comparator.comparing(Student::getId);
         System.out.println(byId);
+
+        //Anonymous class
+        //Runnable runnable = new Runnable() {
+        //  @Override
+        //public void run() {
+        //  System.out.println("Anonymous class implementation");
+        //}
+        //};
+
+        //Lambda Expression
+
+        //Runnable runnable = () -> System.out.println("Lambda Expression implementation");
+
+        RunnableInstance runnableInstance = new RunnableInstance();
+        runnableInstance.doSomething(() -> System.out.println("Lambda Expression implementation")); //Without runnable...
+
+        //Parallel programming with Lambda Expression
+
+        List<String> names = students.stream()
+                .map(s -> s.getName().toUpperCase())
+                .sorted()
+                .collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+        //Counting Odd IDs
+
+        long count = 0;
+        for(Student s: students){
+            if (s.getId() % 2 == 1){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        //With Lambda Expression
+
+        long counts = students.stream()
+                .filter(s -> s.getId() % 2 ==1)
+                .count();
+        System.out.println(counts);
 
     }
 
@@ -23,6 +65,10 @@ public class LambdaExpressions {
     public static class Student{
         private int id;
         private String name;
+        public Student (int id, String name){
+            this.id = id;
+            this.name = name;
+        }
 
         public int getId() {
             return id;
@@ -40,5 +86,28 @@ public class LambdaExpressions {
             this.name = name;
         }
     }
+
+    //
+    //Lambda Expression vs Anonymous Class
+    //
+
+    public static class RunnableInstance{
+        public  void doSomething (Runnable runnable){
+            runnable.run();
+        }
+    }
+
+    private static List<Student> getStudent(){
+        return List.of(
+                new Student(159,"Isaac"),
+                new Student(456, "Luis"),
+                new Student(852, "Rocio"),
+                new Student(951,"Anaheim"),
+                new Student(753, "Rodrigo"),
+                new Student(357, "Samuel"),
+                new Student(654, "Elizabeth")
+        );
+    }
+
 
 }
